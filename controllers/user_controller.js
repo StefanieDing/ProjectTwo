fdef06f4c470d6aeeebe4213f96a19ce0868fdc6
Stefanie Ding <StefanieDing@Stefanie-Dings-MacBook-Pro.local>
2016-11-19 14:07:19 -0500

    edit the controllers

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -22,21 +22,3 @@ router.delete('/delete/reservation/:id', function (req, res){
   //user can delete reservation
 });
 
-//manager end
-router.get('/login/manager', function (req, res){
-  //user auth for manager
-});
-
-router.post('/create/manager', function (req, res){
-  //depending how api works, enters in reservations and spots available
-});
-
-router.put('/update/manager/:id', function(req, res){
-  //allows manager to update calendar
-});
-
-router.delete('/delete/manager/:id', function(req, res){
-  //allows manager to delete event on calendar
-});
-
-//include option where manager can approve event before it is set.
