ec10bd3eed758b5bf835e53b764f222fb3481446
Matthew Mandel <matthewmandel@Matthew-Mandels-MacBook-Pro.local>
2016-11-20 22:02:24 -0500

    updated user controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,44 +1,52 @@
+
 var express = require('express');
 var router = express.Router();
-// var calender = require('../models/calender.js');
 
-router.get('/reserve', function (req, res){
+var calender = require('../models/events.js')
+
+router.get('/', function (req, res){
   //asks to book a reservation or login with manager
-});
-
-// router.get('/calender', function(req, res){
-//   calender.findAll().then(function(data){
-//   var calenderUnderRouterGetTwo = {table: id,
-//                 table: name,
-//                 table: time,
-//                 table: duration
-//                   };
-//   res.render('index', calenderUnderRouterGetTwo);
-//   });
-// });
-
-var stuff = require('../models/events.js');
-
-router.get('/events', function(req, res){
-  stuff.findAll().then(function(data){
-  res.render('index', data);
-  });
+  res.redirect('/reserve')
 });
 
 router.get('/reserve', function (req, res){
   //displays calendar of available dates
+  calender.findAll ({}).then(function(data){
+    var calenderUnderRouterGetTwo = {
+      customers: id,
+      customers: name,
+      customers: phone,
+      customers: email,
+      customers: numOfGuest,
+      customers: createdAt,
+      customers: updatedAt,
+    };
+    res.render('index', calenderUnderRouterGetTwo);
+  }) 
 });
 
 //user end
 router.post('/create/reservation', function (req, res){
   //takes in the information user inputs to reserve a booking
+  var newDate = req.body. '';
+  calenderInfoUnderRouterPost.create({
+    calender_DB:newData
+  });
+  res.redirect('/reserve')
 });
 
 router.put('/update/reservation/:id', function (req, res){
   //user can update the information of reservation
+  calenderInfoUnderRouterPut.update({
+    booked: [req.body.booked],
+},{
+  where:{
+    time: [req.params.id]
+  }
+}
 });
 
 router.delete('/delete/reservation/:id', function (req, res){
   //user can delete reservation
-});
 
+});
