d90b7b8d3004493d92d90f4efe7a345247daf5c8
Kevin Ding <Kevin Ding>
2016-11-20 21:09:58 -0500

    updated user_controller.js

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,52 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-var Event = require('..')
+var Event = require('../models')['events'];
+var Customer = require('../models')['customers'];
 
 router.get('/', function (req, res){
   //asks to book a reservation or login with manager
-  res.redirect('/reserve')
+  res.render('index');
 });
 
 router.get('/reserve', function (req, res){
   //displays calendar of available dates
   calender.findAll ({}).then(function(data){
-    var calenderUnderRouterGetTwo = {
-      customers: id,
-      customers: name,
-      customers: phone,
-      customers: email,
-      customers: numOfGuest,
-      customers: createdAt,
-      customers: updatedAt,
+    res.render('customer'/*handlebars file*/, data);
     };
-    res.render('index', calenderUnderRouterGetTwo);
+    
   }) 
 });
 
-//user end
+//takes in the information user inputs to reserve a booking
 router.post('/create/reservation', function (req, res){
-  //takes in the information user inputs to reserve a booking
-  var newDate = req.body. '';
-  calenderInfoUnderRouterPost.create({
-    calender_DB:newData
+  
+  Customer.create({
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email,
+    numOfGuests: req.body.numOfGuests
   });
-  res.redirect('/reserve')
+  //send to update isPending to true
+  res.redirect('/update/reservation/:id');
 });
 
 router.put('/update/reservation/:id', function (req, res){
   //user can update the information of reservation
-  calenderInfoUnderRouterPut.update({
-    booked: [req.body.booked],
+  Event.update({
+    isPending: [req.body.isPending] //updates pending to true after customer info is entered
 },{
   where:{
-    time: [req.params.id]
+    id: [req.params.id]
   }
 }
 });
 
 router.delete('/delete/reservation/:id', function (req, res){
   //user can delete reservation
+  Customer.destroy({
+    where: {
+      id: [req.params.id]
+    }
+  });
 
+  res.redirect('/reservation');
 });
 
