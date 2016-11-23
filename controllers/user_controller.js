b26a83755cc1fbd54c2110464f0e162e97082ade
Stefanie Ding <StefanieDing@Stefanie-Dings-MacBook-Pro.local>
2016-11-23 18:36:22 -0500

    fixed merge conflict

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,11 +1,17 @@
-
 var express = require('express');
 var router = express.Router();
-// var app = express();
+
 var Event = require('../models')['events'];
 var User = require('../models')['users'];
 
-//-----
+
+//index route
+router.get('/', function (req, res){
+  //asks to book a reservation or login with manager
+  res.redirect('/reserve')
+});
+
+//LOGIN
 router.get('/login', loginGetRoute){
   //the above line ended with a ; instead of { in the guide.This applies to all authentication related routes.
 
@@ -45,6 +51,7 @@ router.post('/login', loginPostRoute){
   }
 }
 
+//LOGOUT
 //we will need to make a log out page or modal for the below route.
 router.get('/logout', logout){
   function logout(req, res){
@@ -62,151 +69,71 @@ router.get('/manager', requireAuth, adminHandler){
     if()
   }
 }
-//-----
 
-<<<<<<< HEAD
-var calender = require('../models/events.js')
-
-router.get('/', function (req, res){
-  //asks to book a reservation or login with manager
-  res.redirect('/reserve')
-=======
-// var methodOverride = require('method-override');
-// app.use(methodOverride('_method'));
-router.get('/', function (req, res){
-  //asks to book a reservation or login with manager
-  res.render('index');
->>>>>>> 30837fafda901536a1e12ddef1b29d23de90940f
-});
-
+//displays calendar of available dates
 router.get('/reserve', function (req, res){
-  //displays calendar of available dates
-<<<<<<< HEAD
-  calender.findAll ({}).then(function(data){
-    var calenderUnderRouterGetTwo = {
-      customers: id,
-      customers: name,
-      customers: phone,
-      customers: email,
-      customers: numOfGuest,
-      customers: createdAt,
-      customers: updatedAt,
-    };
-    res.render('index', calenderUnderRouterGetTwo);
-  }) 
-=======
-  // calender.findAll ({}).then(function(data){
+  Event.findAll ({}).then(function(data){
     res.render('calendarPage');
-  //   };
-  // });
->>>>>>> 30837fafda901536a1e12ddef1b29d23de90940f
+    };
+  });
 });
 
 //takes in the information user inputs to reserve a booking
 router.post('/create/reservation', function (req, res){
-<<<<<<< HEAD
-  //takes in the information user inputs to reserve a booking
-  var newDate = req.body. '';
-  calenderInfoUnderRouterPost.create({
-    calender_DB:newData
-  });
-  res.redirect('/reserve')
-=======
-
   User.create({
     name: req.body.name,
     phone: req.body.phone,
     email: req.body.email,
-    numOfGuests: req.body.numOfGuests
   });
-  //send to update isPending to true
-  res.redirect('/update/reservation/:id');
->>>>>>> 30837fafda901536a1e12ddef1b29d23de90940f
+  //updates reservation and decreases available spots
+  res.redirect('/update/reservation/:id/:spots');
 });
 
-router.put('/update/reservation/:id', function (req, res){
-  //user can update the information of reservation
-<<<<<<< HEAD
-  calenderInfoUnderRouterPut.update({
-    booked: [req.body.booked],
-},{
-  where:{
-    time: [req.params.id]
-  }
-}
-=======
+//user can update the information of reservation
+router.put('/update/reservation/:id/:spots', function (req, res){
   Event.update({
-    isPending: [req.body.isPending] //updates pending to true after customer info is entered
+    availableSpots: (req.body.spots) - 1
 },{
   where:{
-    id: [req.params.id]
+    time: req.params.id
   }
-});
->>>>>>> 30837fafda901536a1e12ddef1b29d23de90940f
-});
-
-router.delete('/delete/reservation/:id', function (req, res){
-  //user can delete reservation
-<<<<<<< HEAD
-
-});
-=======
-  Customer.destroy({
-    where: {
-      id: [req.params.id]
-    }
-  });
-
-  res.redirect('/reservation');
+ });
 });
 
-//manager login
-router.get('/login/manager', function (req, res){
-  //user auth for manager
-  //login
-  //res.redirect('/manager');
-});
+//user can delete reservation
+// router.delete('/delete/reservation/:id', function (req, res){
+  
+//   Customer.destroy({
+//     where: {
+//       id: req.params.id
+//     }
+//   });
 
-//manager homepage
-router.get('/manager', function (req, res){
-  Event.findAll({
-    attributes: ['name', 'date', 'startTime', 'endTime', 'location', 'availableSpots']
-  }).then(function(data){
-    res.render('manager', data);
-  })
-});
+//   res.redirect('/reservation');
+// });
 
 //get customer information for the reservation
 router.get('/customerInfo/:id', function(req, res){
   Customer.findAll({
     where: {
-      id: [req.params.id]
+      id: req.params.id
     }
   });
 });
 
-//MANAGER VIEW
-//enters in event details and spots available
-router.post('/create/manager', function (req, res){
-  Event.create({
-    name: [req.body.name],
-    date: [req.body.date],
-    startTime: [req.body.startTime],
-    endTime: [req.body.endTime],
-    location: [req.body.location],
-    availableSpots: [req.body.availableSpots]
-  });
-
-  res.redirect('/manager');
-});
 
 //allows manager to update calendar
 router.put('/update/manager/:id', function(req, res){
   Event.update({
-    //updated inputs
+    name: req.body.name,
+    date: req.body.date,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+    location: req.body.location,
+    availableSpots: req.body.availableSpots
   },{
     where: {
-      id: [req.params.id]
+      id: req.params.id
     }
   });
 
@@ -214,24 +141,24 @@ router.put('/update/manager/:id', function(req, res){
 });
 
 //manager approves reservation
-router.put('/update/event/"id', function(req, res){
-  Event.update({
-    isPending: //false,
-    isReserved: //true
-  },{
-    where: {
-      id: [req.params.id]
-    }
-  });
+// router.put('/update/event/:id', function(req, res){
+//   Event.update({
+//     isPending: //false,
+//     isReserved: //true
+//   },{
+//     where: {
+//       id: [req.params.id]
+//     }
+//   });
 
-  res.redirect('/manager');
-});
+//   res.redirect('/manager');
+// });
 
 //allows manager to delete event on calendar
 router.delete('/delete/manager/:id', function(req, res){
   Event.destroy({
     where: {
-      id: [req.params.id]
+      id: req.params.id
     }
   });
 
@@ -239,4 +166,4 @@ router.delete('/delete/manager/:id', function(req, res){
 });
 
 module.exports = router;
->>>>>>> 30837fafda901536a1e12ddef1b29d23de90940f
+
