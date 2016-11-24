f48c38a4db756e940f49c408ede277745d010d18
Gerardo Mestanza <Gerardo Mestanza>
2016-11-23 20:35:15 -0500

    fixed td in calendar/now model will pop up if a user clicks on a valid date

--- public/calendar/calendar.js
+++ public/calendar/calendar.js
@@ -100,8 +100,28 @@ var CALENDAR = function () {
 
 };
 
-// $('#modal1').modal('open');
-//when you click on a date an alert pops up, when a date is click prompt a model(materlize??) asking to fill reservation the save in array? or mysql?
-$('td').click(function(){
-	$('.modal').modal();
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//this gets any td with a class name of "calDateData"
+$('.calDateData').click(function(){
+	console.log("hey its me", this)
+		modal.style.display = "block";
 });
